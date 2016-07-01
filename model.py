6182cae6c9d28a1f5d3540f53faf577ab675b885
Lianne Achilles <achilles.lianne@gmail.com>
2016-07-01 14:27:22 -0400

    Initial commit

---
+++ model.py
@@ -0,0 +1,15 @@
+"""Models and database functions for Udemy project"""
+
+from flask_sqlaclemy import SQLAlchemy
+
+# This is the connection to the PostgreSQL database; we're getting this through
+# the Flask-SQLAlchemy helper library. On this, we can find the `session`
+# object, where we do most of our interactions (like committing, etc.)
+
+db = SQLAlchemy()
+
+
+##############################################################################
+# Model definitions
+
+class
